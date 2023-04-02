326afade7621ce14c87141e12c2d675f43aaccf4
Vyacheslav <vya.polyanskiy@gmail.com>
2023-04-02 23:07:44 +0300

    ready program

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iomanip>
 #include "LinkedList.h"
 
-
 double find_cos (double x, double e=0.01, int k = 1, double tmp = 1, double res = 1){
     tmp = -res * tmp / fabs(tmp) * x*x / ((2*k) * (2*k -1));
     res += tmp;
@@ -11,9 +10,23 @@ double find_cos (double x, double e=0.01, int k = 1, double tmp = 1, double res 
     if (fabs(tmp) > e)  res = find_cos(x, e, k+1, tmp, res);
     return res;
 }
+
+bool is_palindrom(std::pair<std::string, std::string> p){
+    return p.first == p.second;
+}
+
+std::pair<std::string, std::string> check_palindrom(Node *node){
+    static std::string s_start = "", s_end="";
+    s_start += node->data;
+    if (node->next != nullptr)    check_palindrom(node->next);
+    s_end += node->data;
+    return {s_start, s_end};
+}
 int main(){
-    LinkedList t = LinkedList("1234re");
-    t.show_list();
+    LinkedList t = LinkedList("qwertyytrewq");
+    //std::cout << std::boolalpha; //output true/false
+    std::cout << is_palindrom(check_palindrom(t.head));
+    //std::cout << a.first << ' ' << a.second << '\n';
     //std::cout << std::setprecision(2); //ouput 2 num after dot
     //std::cout << (find_cos(0, 0.01)) << '\n';
     return 0;
